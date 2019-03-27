46730777f2db64f6c44bcaa28b8cda49b393aabf
andersonlemos <anderson.lemos@dmorj.com.br>
2019-03-27 17:38:24 -0300

    32 - (UPDATE - EVENT EMITTER) Continuando a API - Restify & Mongoose

--- meat-api/common/router.ts
+++ meat-api/common/router.ts
@@ -1,10 +1,12 @@
 import * as restify from 'restify'
+import {EventEmitter} from 'events'
 
-export abstract class Router {
+export abstract class Router extends EventEmitter{
   abstract applyRoutes(application : restify.Server)
   render(response:restify.Response,next:restify.Next){
     return (document)=>{
       if(document){
+        this.emit('beforeReader',document)
         response.send(document)
       }else{
         response.send(404)
